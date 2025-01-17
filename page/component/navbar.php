249b9205c5bd562d7c9d257e1f3f643c18781d26
akaz-cyber <ajrisonic62@gmail.com>
2025-01-17 15:32:57 +0700

    tampilan ketika memencet lapor akan autentifikasi terlebih dahulu

--- page/component/navbar.php
+++ page/component/navbar.php
@@ -1,17 +1,31 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="index.php">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="loginMultiuser/login.php">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="loginMultiuser/register.php">Registrasi</a>
-        </li>
+        <?php if (!isset($_SESSION['id_user'])): ?>
+          <!-- Tampilkan Login dan Registrasi jika belum login -->
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="loginMultiuser/login.php">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="loginMultiuser/register.php">Registrasi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="page/lapor.php">Lapor</a>
+          </li>
+        <?php else: ?>
+          <!-- Tampilkan menu lain jika sudah login -->
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="lapor.php">Lapor</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="../loginMultiuser/logout.php">Logout</a>
+          </li>
+        <?php endif; ?>
       </ul>
     </div>
   </div>
