7086544c37def2eb9f5d3dd9c53788a5467243f9
Alam12346 <syukursidiqnuralam@gmail.com>
2025-01-18 00:22:56 +0700

    login

--- loginMultiuser/login.php
+++ loginMultiuser/login.php
@@ -1,19 +1,45 @@
 <!DOCTYPE html>
 <!-- monggo front end na -->
 <html>
+
 <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
     <title>Login</title>
+  
 </head>
+
 <body>
-    <h2>Login</h2>
-    <form action="../process/process_login.php" method="POST">
-        <label>Email:</label>
-        <input type="email" name="email" required><br><br>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 
-        <label>Password:</label>
-        <input type="password" name="password" required><br><br>
-
-        <button type="submit">Login</button>
-    </form>
+	<main class="container mt-5">
+    <section class="container my-5">
+    <div class="row align-items-center">
+    <div class="col-md-6 text-center">
+    <img src="../page/img/login.png" alt="Lapor" class="img-fluid" style="height: 80%;">
+    </div>
+    <div class="col-md-6 " >
+        <p ><h2>Silahkan masuk terlebih dahulu untuk melapor</h2></p>
+        <form action="../process/process_login.php" method="POST">
+            <div class="form-group"> <label for="email">Email:</label> <input type="email" class="form-control"
+                    id="email" name="email" required> </div>
+            <div class="form-group"> <label for="password">Kata Sandi:</label> <input type="password" class="form-control"
+                    id="password" name="password" required> </div> <button type="submit"
+                class="btn btn-danger mt-5 w-100">Login</button>
+        </form></div>
+    </div></div>
+    </section>
+</main>
+  
+    <?php include '../page/component/footer.php'; ?>
 </body>
+
 </html>
