0634e941ea2cf6066bab1de4a078dbf63a73ffa8
Alam12346 <syukursidiqnuralam@gmail.com>
2025-01-21 19:17:55 +0700

    artikel fix

--- page/artikel.php
+++ page/artikel.php
@@ -49,13 +49,13 @@
 
 
 	<div class="container my-5">
-		<h3 class="mb-5">/Berita dan Informasi</h3>
+		<h3 class="mb-5">Berita dan Informasi</h3>
 
 		<!-- Section 1: Gambar besar -->
 		<div class="row mb-4">
 			<!-- Gambar besar di kiri -->
 			<div class="col-md-8 mb-5">
-				<img src="../page/img/eminem.jpg" class="img-fluid rounded img-main" alt="Main news image" />
+				<img src="/LaporWeb/page/img/eminem.jpg" class="img-fluid rounded img-main" alt="Main news image" />
 				<h4 class="mt-3">Pasar Besar Ngawi Tersangka Korupsi JMK</h4>
 				<p class="text-muted">By NEWSPORTAL.ID • 07 Jan, 2025</p>
 				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
@@ -65,7 +65,7 @@
 			<div class="col-md-4">
 				<!-- Thumbnail Card 1 -->
 				<div class="card-thumbnail d-flex align-items-center mb-3">
-					<img src="../page/img/eminem.jpg" alt="Thumbnail 1" class="rounded">
+					<img src="/LaporWeb/page/img/eminem.jpg" alt="Thumbnail 1" class="rounded">
 					<div class="ms-3">
 						<p class="mb-1 fw-bold text-truncate-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry... </p>
 						<button class="btn btn-sm btn-red">Selengkapnya</button>
@@ -73,7 +73,7 @@
 				</div>
 				<!-- Thumbnail Card 2 -->
 				<div class="card-thumbnail d-flex align-items-center mb-3">
-					<img src="../page/img/news2.jpeg" alt="Thumbnail 2" class="rounded">
+					<img src="/LaporWeb/page/img/news2.jpeg" alt="Thumbnail 2" class="rounded">
 					<div class="ms-3">
 						<p class="mb-1 fw-bold text-truncate-2">Lorem Ipsum is simply dummy text of the printing</p>
 						<button class="btn btn-sm btn-red">Selengkapnya</button>
@@ -81,7 +81,7 @@
 				</div>
 				<!-- Thumbnail Card 3 -->
 				<div class="card-thumbnail d-flex align-items-center mb-3">
-					<img src="../page/img/news2.jpeg" alt="Thumbnail 3" class="rounded">
+					<img src="/LaporWeb/page/img/news2.jpeg" alt="Thumbnail 3" class="rounded">
 					<div class="ms-3">
 						<p class="mb-1 fw-bold text-truncate-2">Lorem Ipsum is simply dummy text of the printing</p>
 						<button class="btn btn-sm btn-red">Selengkapnya</button>
@@ -94,7 +94,7 @@
 		<div class="row gy-4">
 			<div class="col-md-4">
 				<div class="card">
-					<img src="../page/img/news2.jpeg" alt="News 1" class="img-fluid rounded">
+					<img src="/LaporWeb/page/img/news2.jpeg" alt="News 1" class="img-fluid rounded">
 					<div class="card-body">
 						<h5 class="card-title">Lorem Ipsum is simply dummy text</h5>
 						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
@@ -104,7 +104,7 @@
 			</div>
 			<div class="col-md-4">
 				<div class="card">
-					<img src="../page/img/news2.jpeg" alt="News 2" class="img-fluid rounded">
+					<img src="/LaporWeb/page/img/news2.jpeg" alt="News 2" class="img-fluid rounded">
 					<div class="card-body">
 						<h5 class="card-title">Lorem Ipsum is simply dummy text</h5>
 						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
@@ -114,7 +114,7 @@
 			</div>
 			<div class="col-md-4">
 				<div class="card">
-					<img src="../page/img/news2.jpeg" alt="News 3" class="img-fluid rounded">
+					<img src="/LaporWeb/page/img/news2.jpeg" alt="News 3" class="img-fluid rounded">
 					<div class="card-body">
 						<h5 class="card-title">Lorem Ipsum is simply dummy text</h5>
 						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
