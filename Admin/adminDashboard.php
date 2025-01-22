57ff989c78358ba2e399a1586764cdb7cfae543a
Alam12346 <syukursidiqnuralam@gmail.com>
2025-01-22 09:27:06 +0700

    admin dashboard

--- Admin/adminDashboard.php
+++ Admin/adminDashboard.php
@@ -13,13 +13,61 @@ $username = $_SESSION['username'];
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Dashboard Admin</title>
 </head>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'Admin/componen/adminHeader.php'; ?>
+
 <body>
-    <h1>Selamat Datang, Admin <?php echo htmlspecialchars($username); ?>!</h1>
-    <a href="logout">Logout</a> <br> <br>
-    <a href="adminLapor">Lapor</a>
-    <a href="kelolauser">Kelola user</a>
+    <div class="d-flex vh-100">
+
+        <?php include 'Admin/componen/adminSidebar.php'; ?>
+
+        <div class="main-content w-100">
+    <div class="container-fluid p-4">
+        <h4>Dashboard</h4>
+
+        <div class="card mt-3 mx-auto " style="max-width: 600px;" >
+            <div class="card-header bg-danger text-white text-center fw-bold rounded">
+                Selamat datang di halaman  <?php echo htmlspecialchars($username); ?>
+            </div>
+        </div>
+
+        <div class="row mt-4 g-3">
+            <div class="col-md-6">
+                <div class="card text-center shadow-sm">
+                    <div class="card-body"><a href="kelolauser">
+                        <h5 class="card-title">Pengguna</h5>
+                        <p class="display-4 fw-bold">100</p>
+                        <img src="/LaporWeb/page/img/pengguna.png" class="img-fluid" style="max-height: 80px;" alt="Pengguna Icon">
+                        </a></div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card text-center shadow-sm">
+                    <div class="card-body"> <a href="adminLapor">
+                        <h5 class="card-title">Laporan</h5>
+                        <p class="display-4 fw-bold">200</p>
+                         <img src="/LaporWeb/page/img/logolaporan.png" class="img-fluid" style="max-height: 80px;" alt="Laporan Icon">
+                         </a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
--- page/artikel.php
+++ page/artikel.php
@@ -5,7 +5,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Berita dan Informasi</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 	<style>
 		.img-main {
 			object-fit: cover;
@@ -43,10 +44,11 @@
 			-webkit-box-orient: vertical;
 		}
 	</style>
+	
 </head>
 
 <body>
-
+<?php include 'component/navbar.php'; ?>
 
 	<div class="container my-5">
 		<h3 class="mb-5">Berita dan Informasi</h3>
@@ -125,9 +127,10 @@
 		</div>
 	</div>
 
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-
+	<?php include 'page/component/footer.php'; ?>
+	
 </body>
 
 </html>
